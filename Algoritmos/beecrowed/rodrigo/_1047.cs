a4177ecec7d881013cc5f5d0042dd08ce6c54b6b
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-02 17:08:35 -0300

    resolução do problema 1047 do beecrowd

---
+++ Algoritmos/beecrowed/rodrigo/_1047.cs
@@ -0,0 +1,36 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public class _1047
+    {
+        public static void DuracaoEmHorasEMinutos()
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int horaInicial = Convert.ToInt32(entrada[0]);
+            int minutoInicial = Convert.ToInt32(entrada[1]);
+
+            int horaFinal = Convert.ToInt32(entrada[2]);
+            int minutoFinal = Convert.ToInt32(entrada[3]);
+
+            int duracaoHoras, duracaoMinutos;
+            int minutosInicial = horaInicial * 60 + minutoInicial;
+            int minutosFinal = horaFinal * 60 + minutoFinal;
+
+            if (minutosInicial < minutosFinal)
+            {
+                int duracaoMinutosTotal = minutosFinal - minutosInicial;
+                duracaoHoras = duracaoMinutosTotal / 60;
+                duracaoMinutos = duracaoMinutosTotal % 60;
+            }
+            else
+            {
+                int duracaoMinutosTotal = (24 * 60 - minutosInicial) + minutosFinal;
+                duracaoHoras = duracaoMinutosTotal / 60;
+                duracaoMinutos = duracaoMinutosTotal % 60;
+            }
+
+
+            Console.WriteLine($"O JOGO DUROU {duracaoHoras} HORA(S) E {duracaoMinutos} MINUTO(S)");
+        }
+    }
+}
