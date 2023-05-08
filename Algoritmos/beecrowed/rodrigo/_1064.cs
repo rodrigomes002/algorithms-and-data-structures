b86d22b9eac7e3828637a08c25d9ca54b902c2b0
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-08 10:09:54 -0300

    resolvendo o problema 1064

---
+++ Algoritmos/beecrowed/rodrigo/_1064.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public class _1064
+    {
+        public static void PositivosEMedia()
+        {
+            int quantidadePositivos = 0;
+            double somaPositivos = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                double valor = double.Parse(Console.ReadLine());
+
+                if (valor > 0)
+                {
+                    quantidadePositivos++;
+                    somaPositivos += valor;
+                }
+            }
+
+            double mediaPositivos = somaPositivos / quantidadePositivos;
+
+            Console.WriteLine($"{quantidadePositivos} valores positivos");
+            Console.WriteLine(mediaPositivos.ToString("F1"));
+        }
+    }
+}
