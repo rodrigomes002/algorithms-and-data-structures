6d78de5529d0d1b9be083714e678011189a60fa7
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-08 10:48:59 -0300

    resolvendo o problema 1070

---
+++ Algoritmos/beecrowed/rodrigo/_1070.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public class _1070
+    {
+        public static void ImparesConsecutivos()
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int count = 6;
+
+            while (count > 0)
+            {
+                if (numero % 2 > 0)
+                {
+                    Console.WriteLine(numero);
+                    count--;
+                }
+                numero++;
+            }
+        }
+    }
+}
