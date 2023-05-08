12d90a132fa2dc4d541d9bb64e01d4e9d4d542e4
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-08 10:31:17 -0300

    resolvendo o problema 1067

---
+++ Algoritmos/beecrowed/rodrigo/_1067.cs
@@ -0,0 +1,17 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public class _1067
+    {
+        public static void Impares()
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numero; i++)
+            {
+                if (i % 2 > 0)
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
