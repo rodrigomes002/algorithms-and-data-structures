aef0245af822ee356a9440949d1cebd4fc2fe531
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-08 10:14:24 -0300

    resolvendo o problema 1065

---
+++ Algoritmos/beecrowed/rodrigo/_1065.cs
@@ -0,0 +1,19 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public class _1065
+    {
+        public static void ParesEntreNumeros()
+        {
+            int quantidadePares = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                    quantidadePares++;
+            }
+            Console.WriteLine("{0} valores pares", quantidadePares);
+        }
+    }
+}
