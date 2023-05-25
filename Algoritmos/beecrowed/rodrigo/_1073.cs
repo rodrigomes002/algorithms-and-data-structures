c0871cc04851e8328c2cac621d7867caaa06e00d
rodrigomes002 <rodrigomes002@gmail.com>
2023-05-25 18:31:58 -0300

    resolvendo problemas 1072 e 1073

---
+++ Algoritmos/beecrowed/rodrigo/_1073.cs
@@ -0,0 +1,19 @@
+﻿namespace Algoritmos.beecrowed.rodrigo
+{
+    public static class _1073
+    {
+        public static void QuadradoDePares()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int quadrado = i * i;
+                    Console.WriteLine($"{i}^2 = {quadrado}");
+                }
+            }
+        }
+    }
+}
