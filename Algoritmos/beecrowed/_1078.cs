65e34ad59fb4a0b7d6be937a644c96b4b43697c6
rodrigomes002 <rodrigomes002@gmail.com>
2024-01-25 20:09:48 -0300

    resolvendo exercicio 1078

---
+++ Algoritmos/beecrowed/_1078.cs
@@ -0,0 +1,14 @@
+﻿namespace Algoritmos.beecrowed;
+
+public class _1078
+{
+    public static void Tabuada()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{i} x {n} = {n * i}");
+        }
+    }
+}
