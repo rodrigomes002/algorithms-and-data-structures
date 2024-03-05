a85f3c1a86c64f1d628f355527c70d4a31a6b528
rodrigomes002 <rodrigomes002@gmail.com>
2024-03-05 11:18:33 -0300

    exemplo de pilha em c#

---
+++ c#/EstruturaDeDados/Pilha.cs
@@ -0,0 +1,44 @@
+﻿namespace EstruturaDeDados;
+public class Pilha
+{
+    private string[] Itens;
+    private int Topo;
+
+    public Pilha(int capacidade)
+    {
+        Itens = new string[capacidade];
+        Topo = -1;
+    }
+
+    public void Push(string item)
+    {
+        if (Topo == Itens.Length - 1)
+            throw new InvalidOperationException("Pilha cheia");
+
+        Topo++;
+        Itens[Topo] = item;
+    }
+
+    public string Pop()
+    {
+        if (EstaVazia())
+            throw new InvalidOperationException("Pilha vazia");
+
+        string item = Itens[Topo];
+        Topo--;
+        return item;
+    }
+
+    public string Peek()
+    {
+        if (EstaVazia())
+            throw new InvalidOperationException("Pilha vazia");
+
+        return Itens[Topo];
+    }
+
+    public bool EstaVazia()
+    {
+        return Topo == -1;
+    }
+}
